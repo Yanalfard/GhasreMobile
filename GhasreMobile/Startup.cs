328b9ee5a00c120659fd026c12bdca7fbaf6ca1e
hadi <hadisdegh@yahoo.com>
2021-02-04 13:06:10 +0330

    CreateAdminAreas

--- GhasreMobile/Startup.cs
+++ GhasreMobile/Startup.cs
@@ -50,9 +50,15 @@ namespace GhasreMobile
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
 }
+
