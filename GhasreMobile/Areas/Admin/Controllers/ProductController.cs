37414a5b752addf100f08841f4a6d49ecab50a93
hadi <hadisdegh@yahoo.com>
2021-02-04 16:28:59 +0330

    UpdateAdminAreas

---
+++ GhasreMobile/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GhasreMobile.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
