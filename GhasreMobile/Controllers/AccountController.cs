af8d7475f5f9c5b558847a5f7679da5c55c025c8
Lemi <Lemi@DESKTOP-SDD69VQ>
2021-02-09 11:29:40 +0330

    Search Page, Forum, Blogs, -Account Progress

---
+++ GhasreMobile/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GhasreMobile.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Login
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Register
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ GhasreMobile/Controllers/Forum.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GhasreMobile.Controllers
+{
+    public class Forum : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ForumView()
+        {
+            return View();
+        }
+
+    }
+}
