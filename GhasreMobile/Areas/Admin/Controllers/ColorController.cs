a1a352f8ce6fa3e7b14c1d4df81d9f34e51fdffe
hadi <hadisdegh@yahoo.com>
2021-02-14 10:33:21 +0330

    AddCkEditor

--- GhasreMobile/Areas/Admin/Controllers/ColorController.cs
+++ GhasreMobile/Areas/Admin/Controllers/ColorController.cs
@@ -3,14 +3,51 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer.Models;
+using Services.Services;
 
 namespace GhasreMobile.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ColorController : Controller
     {
-        public IActionResult Index()
+        Core _core = new Core();
+        public IActionResult Index(int page = 1)
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return ViewComponent("CreateColor");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(TblColor color)
+        {
+            if (ModelState.IsValid)
+            {
+                _core.Color.Add(color);
+                _core.Color.Save();
+                return Redirect("/Color");
+            }
+            return View(color);
+        }
+
+        public IActionResult Edit(int Id)
+        {
+            return ViewComponent("EditColor", new { Id = Id });
+        }
+
+        public IActionResult Edit(TblColor color, int Page)
+        {
+            if (ModelState.IsValid)
+            {
+                _core.Color.Update(color);
+                _core.Color.Save();
+                return Redirect("/Color?page=" + Page);
+            }
+            return View(color);
         }
     }
 }
