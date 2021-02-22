dc8eaa2ae3f6633b155250798844ce7b841d91e8
b3jan <b3jan@DESKTOP-MLGLOK5>
2021-02-22 16:52:11 +0330

    Create delivery index and component

---
+++ GhasreMobile/Areas/Admin/Controllers/DeliveryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GhasreMobile.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DeliveryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            return ViewComponent("DeliveryInfoAdmin");
+        }
+    }
+}
