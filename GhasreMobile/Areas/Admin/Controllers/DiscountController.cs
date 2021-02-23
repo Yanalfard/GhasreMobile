5ab1fbe14d96721f78c82ae53a34396096ab6465
b3jan <b3jan@DESKTOP-MLGLOK5>
2021-02-23 17:19:28 +0330

    create discount and components

---
+++ GhasreMobile/Areas/Admin/Controllers/DiscountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GhasreMobile.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DiscountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return ViewComponent("DiscountCreateAdmin");
+        }
+        public IActionResult Edit()
+        {
+            return ViewComponent("DiscountEditAdmin");
+        }
+    }
+}
