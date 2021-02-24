eb115363da2e8a1024199db0161a15a59c13c9ab
Mehdi Sahandi <mehdisahandi@gmail.com>
2021-02-24 17:56:26 +0330

    update roue page

--- GhasreMobile/Startup.cs
+++ GhasreMobile/Startup.cs
@@ -66,12 +66,13 @@ namespace GhasreMobile
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
             app.UseMvc();
-            app.UseRouting();
+            app.UseStaticFiles();
             app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseRouting();
             app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
