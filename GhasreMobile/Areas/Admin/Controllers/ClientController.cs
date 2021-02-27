7d7b5d1656c11806391a369f5c77c4c0fb1020df
nazila bagheri <nazila.bagheri93@gmail.com>
2021-02-27 14:39:02 +0330

    admin: Client view page

---
+++ GhasreMobile/Areas/Admin/Controllers/ClientController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GhasreMobile.Utilities;
+namespace GhasreMobile.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [PermissionChecker("admin")]
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
