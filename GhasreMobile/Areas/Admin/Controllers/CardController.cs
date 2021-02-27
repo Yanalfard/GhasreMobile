a9b34e6df844d2615fbf6381b599f16216a6b95e
hadi <hadisdegh@yahoo.com>
2021-02-27 10:52:39 +0330

    ChangetblBankAccounts

--- GhasreMobile/Areas/Admin/Controllers/CardController.cs
+++ GhasreMobile/Areas/Admin/Controllers/CardController.cs
@@ -3,23 +3,63 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer.Models;
+using Services.Services;
 
 namespace GhasreMobile.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CardController : Controller
     {
+        Core _core = new Core();
         public IActionResult Index()
         {
-            return View();
+            return View(_core.BankAccounts.Get());
         }
+
+        [HttpGet]
         public IActionResult Create()
         {
             return ViewComponent("CardCreateAdmin");
         }
-        public IActionResult Edit()
+
+        [HttpPost]
+        public IActionResult Create(TblBankAccounts bankAccount)
         {
-            return ViewComponent("CardEditAdmin");
+            if (ModelState.IsValid)
+            {
+                _core.BankAccounts.Add(bankAccount);
+                _core.BankAccounts.Save();
+                return Redirect("/Admin/Card");
+            }
+            return View(bankAccount);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return ViewComponent("CardEditAdmin",_core.BankAccounts.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TblBankAccounts bankAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                _core.BankAccounts.Update(bankAccount);
+                _core.BankAccounts.Save();
+                return Redirect("/Admin/Card");
+            }
+           return View(bankAccount);
+        }
+
+        [HttpPost]
+        public void ChangeStatus(int id)
+        {
+            TblBankAccounts bankAccounts = _core.BankAccounts.GetById(id);
+            bankAccounts.IsActive = !bankAccounts.IsActive;
+            _core.BankAccounts.Update(bankAccounts);
+            _core.BankAccounts.Save();
         }
     }
 }
