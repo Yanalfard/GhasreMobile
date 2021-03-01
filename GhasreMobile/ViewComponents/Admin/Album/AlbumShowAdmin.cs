be306e5515e4d52c7c5cc52089666981019fdf2d
hadi <hadisdegh@yahoo.com>
2021-03-01 15:16:48 +0330

    AlbumController

--- GhasreMobile/ViewComponents/Admin/Album/AlbumShowAdmin.cs
+++ GhasreMobile/ViewComponents/Admin/Album/AlbumShowAdmin.cs
@@ -3,14 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Services.Services;
 
 namespace GhasreMobile.ViewComponents.Admin.Album
 {
     public class AlbumShowAdmin:ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+        Core _core = new Core();
+        public async Task<IViewComponentResult> InvokeAsync(int id)
         {
-            return await Task.FromResult((IViewComponentResult)View("/Areas/Admin/Views/Album/Components/Show.cshtml"));
+            return await Task.FromResult((IViewComponentResult)View("/Areas/Admin/Views/Album/Components/Show.cshtml",_core.Album.GetById(id)));
         }
     }
 }
