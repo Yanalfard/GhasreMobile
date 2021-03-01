f1cf78d175f26805a03d1ee2fd63563e9872f33a
b3jan <b3jan@DESKTOP-MLGLOK5>
2021-03-01 15:10:36 +0330

    topic, special offer, product changed

---
+++ GhasreMobile/ViewComponents/Admin/Topic/TopicInfoAdmin.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GhasreMobile.ViewComponents.Admin.Topic
+{
+    public class TopicInfoAdmin : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("/Areas/Admin/Views/Topic/Components/Info.cshtml"));
+        }
+    }
+}
