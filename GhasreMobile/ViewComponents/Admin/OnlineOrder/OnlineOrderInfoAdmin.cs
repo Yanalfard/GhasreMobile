fdd3bf460b83fa36d1e83b10aba5a292102e0c7f
hadi <hadisdegh@yahoo.com>
2021-03-02 10:11:03 +0330

    ClientController

--- GhasreMobile/ViewComponents/Admin/OnlineOrder/OnlineOrderInfoAdmin.cs
+++ GhasreMobile/ViewComponents/Admin/OnlineOrder/OnlineOrderInfoAdmin.cs
@@ -3,14 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Services.Services;
 
 namespace GhasreMobile.ViewComponents.Admin.OnlineOrder
 {
-    public class OnlineOrderInfoAdmin:ViewComponent
+    public class OnlineOrderInfoAdmin : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+        Core _core = new Core();
+        public async Task<IViewComponentResult> InvokeAsync(int id)
         {
-            return await Task.FromResult((IViewComponentResult)View("/Areas/Admin/Views/OnlineOrder/Components/Info.cshtml"));
+            return await Task.FromResult((IViewComponentResult)View("/Areas/Admin/Views/OnlineOrder/Components/Info.cshtml", _core.OnlineOrder.GetById(id)));
         }
     }
 }
