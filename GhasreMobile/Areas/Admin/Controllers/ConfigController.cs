c78d306e4401bbd097be8d7686ddc229c9d63b23
hadi <hadisdegh@yahoo.com>
2021-03-04 16:03:32 +0330

    AddConfigInAdmin

--- GhasreMobile/Areas/Admin/Controllers/ConfigController.cs
+++ GhasreMobile/Areas/Admin/Controllers/ConfigController.cs
@@ -3,15 +3,61 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer.Models;
+using DataLayer.ViewModels;
+using Services.Services;
+using GhasreMobile.Utilities;
 
 namespace GhasreMobile.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [PermissionChecker("admin")]
     public class ConfigController : Controller
     {
+        Core _core = new Core();
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<TblConfig> configs = _core.Config.Get();
+            ConfigVm config = new ConfigVm();
+            config.DarbareyeMa = configs.Where(c => c.Key == "DarbareyeMa").Single().Value;
+            config.TamasBaMa = configs.Where(c => c.Key == "TamasBaMa").Single().Value;
+            config.HazineErsalBaPost = Convert.ToInt32(configs.Where(c => c.Key == "HazineErsalBaPost").Single().Value);
+            config.HazineErsalBaTipaxChapare = Convert.ToInt32(configs.Where(c => c.Key == "HazineErsalBaTipax/Chapare").Single().Value);
+            config.HazineErsalPeyk = Convert.ToInt32(configs.Where(c => c.Key == "HazineErsalPeyk").Single().Value);
+            return View(config);
         }
+
+        [HttpPost]
+        public IActionResult Index(ConfigVm configVm)
+        {
+            if (ModelState.IsValid)
+            {
+                IEnumerable<TblConfig> configs = _core.Config.Get();
+
+                TblConfig ConfigDarbareyeMa = configs.Where(c => c.Key == "DarbareyeMa").Single();
+                TblConfig ConfigTamasBaMa = configs.Where(c => c.Key == "TamasBaMa").Single();
+                TblConfig ConfigHazineErsalBaPost = configs.Where(c => c.Key == "HazineErsalBaPost").Single();
+                TblConfig ConfigHazineErsalBaTipaxChapare = configs.Where(c => c.Key == "HazineErsalBaTipax/Chapare").Single();
+                TblConfig ConfigHazineErsalPeyk = configs.Where(c => c.Key == "HazineErsalPeyk").Single();
+
+                ConfigDarbareyeMa.Value = configVm.DarbareyeMa;
+                ConfigHazineErsalBaPost.Value = configVm.HazineErsalBaPost.ToString();
+                ConfigHazineErsalBaTipaxChapare.Value = configVm.HazineErsalBaTipaxChapare.ToString();
+                ConfigTamasBaMa.Value = configVm.TamasBaMa;
+                ConfigHazineErsalPeyk.Value = configVm.HazineErsalPeyk.ToString();
+
+                _core.Config.Update(ConfigDarbareyeMa);
+                _core.Config.Update(ConfigTamasBaMa);
+                _core.Config.Update(ConfigHazineErsalBaPost);
+                _core.Config.Update(ConfigHazineErsalBaTipaxChapare);
+                _core.Config.Update(ConfigHazineErsalPeyk);
+
+                _core.Config.Save();
+            }
+            return View(configVm);
+        }
+
     }
 }
