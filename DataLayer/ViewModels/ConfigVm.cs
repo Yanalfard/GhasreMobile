e9f58725ad425f0e66684c895fda42b38dec152c
yanal <yanal.fard@gmail.com>
2021-03-14 12:00:09 +0330

    notifications employee and config update

--- DataLayer/ViewModels/ConfigVm.cs
+++ DataLayer/ViewModels/ConfigVm.cs
@@ -7,20 +7,14 @@ namespace DataLayer.ViewModels
 {
     public class ConfigVm
     {
-        [Required(ErrorMessage = "لطفا هزینه ارسال با پست را کامل کنید")]
-        public int? HazineErsalBaPost { get; set; }
-
-        [Required(ErrorMessage = "لطفا هزینه ارسال با تیپاکس را کامل کنید")]
-        public int? HazineErsalBaTipaxChapare { get; set; }
-
         [Required(ErrorMessage = "لطفا تماس با ما را کامل کنید")]
         public string TamasBaMa { get; set; }
 
+        [Required(ErrorMessage = "لطفا تماس با ما کوتاه را کامل کنید")]
+        public string ShortTamasBaMa { get; set; }
+
         [Required(ErrorMessage = "لطفا درباره ما را کامل کنید")]
         public string DarbareyeMa { get; set; }
-
-        [Required(ErrorMessage = "لطفا هزینه ارسال با پیک را کامل کنید")]
-        public int HazineErsalPeyk { get; set; }
 
         [Required(ErrorMessage = "لطفا توضیحات نمایندگی را کامل کنید")]
         public string StoreDescription { get; set; }
