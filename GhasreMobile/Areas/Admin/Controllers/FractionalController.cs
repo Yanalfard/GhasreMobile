14cc753213c2d9edc7ac5eb2552b13d6a485527b
b3jan <b3jan@DESKTOP-MLGLOK5>
2021-03-14 10:13:47 +0330

    fractional

--- GhasreMobile/Areas/Admin/Controllers/FractionalController.cs
+++ GhasreMobile/Areas/Admin/Controllers/FractionalController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GhasreMobile.Utilities;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,13 @@ using System.Threading.Tasks;
 
 namespace GhasreMobile.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [PermissionChecker("admin,employee")]
     public class FractionalController : Controller
     {
         public IActionResult Index()
         {
             return View();
-        }
+        }        
     }
 }
