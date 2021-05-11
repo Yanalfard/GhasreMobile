8f8136c56504608c527991cedcbf42acd2b74c5f
Mehdi Sahandi <mehdisahandi@gmail.com>
2021-05-11 22:35:24 +0430

    update bogs

--- GhasreMobile/ViewComponents/View/Product/ProductView.cs
+++ GhasreMobile/ViewComponents/View/Product/ProductView.cs
@@ -11,18 +11,24 @@ namespace GhasreMobile.ViewComponents.View.Product
     public class ProductView : ViewComponent
     {
         private Core db = new Core();
-        public async Task<IViewComponentResult> InvokeAsync(int? id = 0)
+        public async Task<IViewComponentResult> InvokeAsync()
         {
             List<TblBrand> list = new List<TblBrand>();
-            if (id == 0)
-            {
-                list = db.Brand.Get(i => i.TblProduct.Any()).OrderBy(i => i.TblProduct.Count()).Skip(1).OrderByDescending(i => i.TblProduct.Count()).ToList();
-            }
-            else
-            {
-                list = db.Brand.Get(i=>i.TblProduct.Any()).OrderBy(i => i.TblProduct.Count()).Take(1).OrderByDescending(i => i.TblProduct.Count()).ToList();
-            }
+            list = db.Brand.Get(i => i.TblProduct.Any()).OrderBy(i => i.TblProduct.Count()).OrderByDescending(i => i.TblProduct.Count()).ToList();
             return await Task.FromResult((IViewComponentResult)View("~/Views/Shared/Components/ProductView/ProductView.cshtml", list));
         }
+        //public async Task<IViewComponentResult> InvokeAsync(int? id = 0)
+        //{
+        //    List<TblBrand> list = new List<TblBrand>();
+        //    if (id == 0)
+        //    {
+        //        list = db.Brand.Get(i => i.TblProduct.Any()).OrderBy(i => i.TblProduct.Count()).Skip(1).OrderByDescending(i => i.TblProduct.Count()).ToList();
+        //    }
+        //    else
+        //    {
+        //        list = db.Brand.Get(i=>i.TblProduct.Any()).OrderBy(i => i.TblProduct.Count()).Take(1).OrderByDescending(i => i.TblProduct.Count()).ToList();
+        //    }
+        //    return await Task.FromResult((IViewComponentResult)View("~/Views/Shared/Components/ProductView/ProductView.cshtml", list));
+        //}
     }
 }
